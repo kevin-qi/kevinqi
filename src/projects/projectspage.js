e961c85a4977bafdc92c1e0f09791a3fde604ab8
Kevin <kevin.qi@berkeley.edu>
2019-10-28 16:30:53 -0700

    Set up routing

---
+++ src/projects/projectspage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './projectspage.css';
+
+class ProjectsPage extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Projects page</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectsPage;
