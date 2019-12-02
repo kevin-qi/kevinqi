6caafe720f3eaad7b0b17d3f787a3217edb59ec6
Kevin <kevin.qi@berkeley.edu>
2019-12-02 13:30:19 -0800

    Updates

---
+++ precache-manifest.8a8e12b688281dcece39d7bd931ad3e6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3951427ae3df8371cb4820915cecdc",
+    "url": "/kevinqi/index.html"
+  },
+  {
+    "revision": "dc6aa78f440621140137",
+    "url": "/kevinqi/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "c669b5b9f4d9f42e7e6f",
+    "url": "/kevinqi/static/css/main.7e50da47.chunk.css"
+  },
+  {
+    "revision": "dc6aa78f440621140137",
+    "url": "/kevinqi/static/js/2.a733baf9.chunk.js"
+  },
+  {
+    "revision": "c669b5b9f4d9f42e7e6f",
+    "url": "/kevinqi/static/js/main.b075965c.chunk.js"
+  },
+  {
+    "revision": "6e944f59e1d65eba53a3",
+    "url": "/kevinqi/static/js/runtime-main.380babf3.js"
+  },
+  {
+    "revision": "d939ce4ccc02a555a4ade68eb9ef939a",
+    "url": "/kevinqi/static/media/CVD_graphene.d939ce4c.jpg"
+  },
+  {
+    "revision": "ee214ec8cce54ba7596328d87e508749",
+    "url": "/kevinqi/static/media/ProteinRank.ee214ec8.jpg"
+  },
+  {
+    "revision": "625486f673c110bb6b6a8942bf67847a",
+    "url": "/kevinqi/static/media/bittermelon_salad.625486f6.jpg"
+  },
+  {
+    "revision": "74bbf9c66e75a0b582166501e903d6b0",
+    "url": "/kevinqi/static/media/graphene_transfer.74bbf9c6.jpg"
+  },
+  {
+    "revision": "54ea01b542fcf862e9d0d4bbb2a3e730",
+    "url": "/kevinqi/static/media/labview.54ea01b5.jpg"
+  },
+  {
+    "revision": "1a66a1dcc5c710aefa522db5f15345a3",
+    "url": "/kevinqi/static/media/linkedin.1a66a1dc.jpg"
+  },
+  {
+    "revision": "4f35be4197b7318affd8ebd4dda10173",
+    "url": "/kevinqi/static/media/mala_dry_pot.4f35be41.jpg"
+  },
+  {
+    "revision": "5cac7732b015d1baac9e1086f76d3031",
+    "url": "/kevinqi/static/media/mancini.5cac7732.jpg"
+  },
+  {
+    "revision": "0cf16491bd47593d99754475e37a97d6",
+    "url": "/kevinqi/static/media/miso_glazed_blackcod.0cf16491.jpg"
+  },
+  {
+    "revision": "8495c8480cee4223104018e0e87e5105",
+    "url": "/kevinqi/static/media/pdms_microscope.8495c848.jpg"
+  },
+  {
+    "revision": "a61541825428e14e84748b148b63ed31",
+    "url": "/kevinqi/static/media/profile.a6154182.jpg"
+  },
+  {
+    "revision": "fdf6de347c0b5e4148066c2da6c5fa61",
+    "url": "/kevinqi/static/media/qPlus_sensor.fdf6de34.jpg"
+  },
+  {
+    "revision": "7489c38e1248bdd1a39dd523beec9ae3",
+    "url": "/kevinqi/static/media/sichuan_cold_noodles.7489c38e.jpg"
+  },
+  {
+    "revision": "b6e3c651b1399f672f0ae54906f411d3",
+    "url": "/kevinqi/static/media/stuffed_bellpeppers.b6e3c651.jpg"
+  },
+  {
+    "revision": "796eb71423b3c5eac7e9f948074e9c6e",
+    "url": "/kevinqi/static/media/sweet_sour_lotusroot.796eb714.jpg"
+  },
+  {
+    "revision": "95617695f47667d8526ef0fe6a74f088",
+    "url": "/kevinqi/static/media/test.95617695.jpg"
+  }
+]);
